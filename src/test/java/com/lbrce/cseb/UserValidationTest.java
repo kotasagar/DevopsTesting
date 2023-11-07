6457b9d9a0f431b8e23f7e84104c2dc86bf49f18
sagar <94432029+kotasagar@users.noreply.github.com>
2023-11-06 21:41:18 -0800

    Add files via upload

---
+++ src/test/java/com/lbrce/cseb/UserValidationTest.java
@@ -0,0 +1,20 @@
+package com.lbrce.cseb;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserValidationTest {
+	@Test
+	public void testCase1()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(false,uv.check("", ""));
+	}
+	@Test
+	public void testCase2()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(true,uv.check("srinu", "srinu@123"));
+	}
+	
+}
